7beef0e6ae0f47465c06d4d2ede6d054acb6ac8e
Akshay Vijay Jain <theakshay.jain@gmail.com>
2025-03-10 14:49:39 +0530

    adding storybook to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
 import App from 'components/App'
 import { applyPolyfills, defineCustomElements } from '@bh-digitalsolutions/ui-toolkit/loader';
 
@@ -8,13 +7,19 @@ import { applyPolyfills, defineCustomElements } from '@bh-digitalsolutions/ui-to
 //   await import('@bh-digitalsolutions/ui-toolkit/dist/esm/bh-a_26.entry.js');
 //   defineCustomElements(window);
 // });
-
-applyPolyfills().then(async () => {
+if (import.meta.env.DEV) {
+  console.log('Development mode');
+  applyPolyfills().then(() => {
+    defineCustomElements();
+  });
+} else {
+  applyPolyfills().then(async () => {
     await import('@bh-digitalsolutions/ui-toolkit/dist/esm/bh-a_26.entry.js');
     await import('@bh-digitalsolutions/ui-toolkit/dist/esm/bh-menu.entry.js');
     await import('@bh-digitalsolutions/ui-toolkit/dist/index.js');
     defineCustomElements(window);
   });
+}
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
